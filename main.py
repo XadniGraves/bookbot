10dde42d2828cce88e50ea7b2591848a01f9bdb6
XadniGraves <xadnigraves@gmail.com>
2025-08-11 00:38:47 -0400

    added get_char_count to stats.py and imported to main.py

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-from stats import get_book_wc
+from stats import get_book_wc, get_char_count    
 
 def get_book_text(book_filepath):
     with open(book_filepath) as book:
         print(book.read())
 
 def main():
-    get_book_wc("books/frankenstein.txt")
+    print(get_book_wc("books/frankenstein.txt"))
+    char_count = get_char_count("books/frankenstein.txt")
+    print (char_count)
 
 main()
--- stats.py
+++ stats.py
@@ -1,4 +1,14 @@
 def get_book_wc(book_filepath):
     with open(book_filepath) as book:
         word_count = len(book.read().split())
-        print(f"{word_count} words found in the document")
+        return f"{word_count} words found in the document"
+
+def get_char_count(book_filepath):
+    with open(book_filepath) as book:
+        char_count = {}
+        for char in book.read():
+            if char.lower() not in char_count:
+                char_count[char.lower()] = 1
+                continue
+            char_count[char.lower()] += 1
+        return dict(sorted(char_count.items()))
