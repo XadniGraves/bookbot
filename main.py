9a03b183115a259fb3fee8a192cbf626123b1703
XadniGraves <xadnigraves@gmail.com>
2025-08-11 01:27:39 -0400

    added ability to pass book filepath as a cli argument

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from stats import get_book_wc, get_char_count, sort_char_count    
+from stats import get_book_wc, get_char_count, sort_char_count  
+import sys  
 
 def get_book_text(book_filepath):
     with open(book_filepath) as book:
@@ -6,11 +7,11 @@ def get_book_text(book_filepath):
 
 def main():
     print("============ BOOKBOT ============")
-    print(f"Analyzing book found at books/frankenstein.txt")
+    print(f"Analyzing book found at {sys.argv[1]}")
     print("----------- Word Count ----------")
-    print(get_book_wc("books/frankenstein.txt"))
+    print(get_book_wc(sys.argv[1]))
     print("--------- Character Count -------")
-    char_count = sort_char_count(get_char_count("books/frankenstein.txt"))
+    char_count = sort_char_count(get_char_count(sys.argv[1]))
     for char in char_count:
         print(f"{char["char"]}: {char["count"]}")
     print("============= END ===============")
